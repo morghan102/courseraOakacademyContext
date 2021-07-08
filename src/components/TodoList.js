6efce2159becaef4340e469416f25cfb45715241
Morghan <morghan102@gmail.com>
2021-07-08 13:15:51 -0700

    change context button

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 
 import {ThemeContext} from '../contexts/ThemeContext';
 
@@ -7,15 +7,18 @@ class TodoList extends Component {
     static contextType = ThemeContext;
 
     render() {
-        const { isDarkTheme, darkTheme, lightTheme } = this.context;
+        const { isDarkTheme, darkTheme, lightTheme, changeTheme } = this.context;
         const theme = isDarkTheme ? darkTheme : lightTheme;
 
-        const { todoContainer, item } = styles;
+        const { todoContainer, item, buttonContainer, buttonText } = styles;
         return (
             <View style={[todoContainer, theme]}>
                 <Text style={[item, theme]}>Plan</Text>
                 <Text style={[item, theme]}>Ahop</Text>
                 <Text style={[item, theme]}>Walk</Text>
+                <TouchableOpacity onPress={changeTheme} style={buttonContainer}>
+                    <Text style={buttonText}>Change Theme</Text>
+                </TouchableOpacity>
             </View>
         );
     }
@@ -31,6 +34,17 @@ const styles = StyleSheet.create({
         color: 'white',
         fontSize: 18,
         paddingVertical: 10
+    },
+    buttonContainer: {
+        backgroundColor: 'dodgerblue',
+        paddingVertical: 10,
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 18
     }
 });
 
