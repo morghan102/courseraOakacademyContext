a170c88b4deb84050adf3178f6614b4e6d5c7728
Morghan <morghan102@gmail.com>
2021-07-09 17:01:49 -0700

    put reducer in own file

--- src/contexts/TodoListContext.js
+++ src/contexts/TodoListContext.js
@@ -1,17 +1,9 @@
 import React, { createContext, useReducer } from 'react';
 
+import { todosReducer } from '../reducers/todosReducer';
+
 export const TodoListContext = createContext();
 
-const todosReducer = (state, action) => {
-    switch (action.type) {
-        case 'ADD_TODO':
-            return [...state, { text: action.text, id: `${Math.random()}` }]
-        case 'REMOVE_TODO':
-            return state.filter((todo) => todo.id !== action.id)
-        default:
-            return state;
-    }
-}
 
 const TodoListContextProvider = ({ children }) => {
     const [todos, dispatch] = useReducer(todosReducer, [
